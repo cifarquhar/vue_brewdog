6bc1ef46579ad11c34a6f5e7f930794ffb44c99b
cifarquhar <colin.farquhar@codeclan.com>
2019-04-19 15:25:23 +0100

    add basic list of beers

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div id="app">
-    <h1>Hello world!</h1>
+    <ul>
+      <li v-for="(beer, index) in allBeers" :key="index">{{beer.name}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'app'
+  name: 'app',
+  data() {
+    return{
+      allBeers: []
+    }
+  },
+  mounted(){
+    fetch("https://api.punkapi.com/v2/beers")
+      .then((res) => res.json())
+      .then((data) => this.allBeers = data);
+  }
 }
 </script>
 
