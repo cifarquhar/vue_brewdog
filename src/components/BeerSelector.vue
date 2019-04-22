cabaca11d97f16d1576e2900b0d2746f36030037
cifarquhar <colin.farquhar@codeclan.com>
2019-04-22 16:25:54 +0100

    add form to selector

--- src/components/BeerSelector.vue
+++ src/components/BeerSelector.vue
@@ -1,6 +1,10 @@
 <template lang="html">
   <div>
     <button v-on:click="handleClick">Get Random Beer</button>
+    <form v-on:submit="handleSubmit">
+      <label for="name">Name: </label><input id="name" name="name" type="text">
+      <input type="submit" value="Search">
+    </form>
   </div>
 </template>
 
@@ -14,6 +18,10 @@ export default {
       fetch("https://api.punkapi.com/v2/beers/random")
         .then((res) => res.json())
         .then((beer) => eventBus.$emit('beer-selected', beer[0]))
+    },
+    handleSubmit: function(evt){
+      evt.preventDefault();
+      console.log("form submitted");
     }
   }
 }
