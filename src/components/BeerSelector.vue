d7339815b4050c1d2b02ee352111094f270d6b8a
cifarquhar <colin.farquhar@codeclan.com>
2019-04-23 10:59:16 +0100

    add EBC to form

--- src/components/BeerSelector.vue
+++ src/components/BeerSelector.vue
@@ -8,11 +8,14 @@
       <input id="min-abv" name="abv_gt" type="number" min=0 placeholder=0>
       <label for="max-abv">Max. ABV: </label>
       <input id="max-abv" name="abv_lt" type="number" min=0 placeholder=0>
-      <input type="submit" value="Search">
       <label for="min-ibu">Min. IBU: </label>
       <input id="min-ibu" name="ibu_gt" type="number" min=0 placeholder=0>
       <label for="max-ibu">Max. IBU: </label>
       <input id="max-ibu" name="ibu_lt" type="number" min=0 placeholder=0>
+      <label for="min-ebc">Min. EBC: </label>
+      <input id="min-ebc" name="ebc_gt" type="number" min=0 placeholder=0>
+      <label for="max-ebc">Max. EBC: </label>
+      <input id="max-ebc" name="ebc_lt" type="number" min=0 placeholder=0>
       <input type="submit" value="Search">
     </form>
   </div>
