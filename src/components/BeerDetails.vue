6cd913228f05480d7750b80b05959398e6c41c59
cifarquhar <colin.farquhar@codeclan.com>
2019-04-24 12:53:57 +0100

    add some beer recipe details

--- src/components/BeerDetails.vue
+++ src/components/BeerDetails.vue
@@ -4,6 +4,39 @@
     <h4>{{beer.tagline}}</h4>
     <p>{{beer.description}}</p>
     <img v-if="beer.image_url" :src="beer.image_url" :alt="beer.name" class="beer-image">
+    <table>
+      <th>
+        <td>Grain</td>
+        <td>Quantity</td>
+      </th>
+      <tr v-for="(ingredient, index) in beer.ingredients.malt" :key="index">
+        <td>{{ingredient.name}}</td>
+        <td>{{`${ingredient.amount.value} kg`}}</td>
+      </tr>
+    </table>
+    <table>
+      <th>
+        <td>Hop</td>
+        <td>Quantity</td>
+        <td>Time</td>
+      </th>
+      <tr v-for="(hop, index) in beer.ingredients.hops" :key="index">
+        <td>{{hop.name}}</td>
+        <td>{{`${hop.amount.value} g`}}</td>
+        <td>{{hop.add}}</td>
+      </tr>
+    </table>
+    <p><strong>Yeast:</strong> {{beer.ingredients.yeast}}</p>
+    <table>
+      <th>
+        <td>Mash:</td>
+      </th>
+      <tr v-for="(stage, index) in beer.method.mash_temp">
+        <td>{{stage.duration ? stage.duration : 60}} mins</td>
+        <td>{{stage.temp.value}} &#8451;</td>
+      </tr>
+    </table>
+    <p><strong>Ferment:</strong> {{beer.method.fermentation.temp.value}} &#8451;</p>
   </div>
 </template>
 
