8332552a540b2967566a1f08b3a21cf3d2d09f79
cifarquhar <colin.farquhar@codeclan.com>
2019-04-24 10:11:49 +0100

    configure bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue);
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import App from './App.vue';
 
 Vue.config.productionTip = false
 
